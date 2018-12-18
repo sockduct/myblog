b8f06dee944d759ad7e8ca76336deb88fe5a356d
James Small <james.r.small@outlook.com>
2018-12-18 06:00:46 -0500

    update:  Add jobexmpl2 with progress tracking

---
+++ jobexmpl2.py
@@ -0,0 +1,17 @@
+import time
+from rq import get_current_job
+
+
+def example(seconds):
+    job = get_current_job()
+    print('Starting task')
+    for i in range(seconds):
+        # This is a way to track progress from rq
+        job.meta['progress'] = 100.0 * i/seconds
+        job.save_meta()
+        print(i)
+        time.sleep(1)
+    job.meta['progress'] = 100
+    job.save_meta()
+    print('Task completed')
+
